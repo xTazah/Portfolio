ae833051fad73eae125ef9ae758ab831278d288c
Tazah <finnbeifong@gmail.com>
2024-11-26 18:39:54 +0100

    update aboutme section

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,32 +1,45 @@
-import { MapPinIcon } from '@heroicons/react/24/outline';
-import { InterestsSection } from './InterestsSection';
+import { MapPinIcon } from "@heroicons/react/24/outline";
+import { motion } from "framer-motion";
+import { wavingAnimation } from "../utils/animations";
 
 export const AboutMe = () => {
   return (
     <>
-    <section className="relative w-full mx-auto">
-    <div className="inset-0 mt-12 max-w-7xl mx-auto px-8 flex flex-row items-start gap-6">
-      
-      {/* Main Content */}
-      <div>
-        <h1 className="text-4xl sm:text-6xl font-bold text-gray-900 dark:text-white leading-tight">
-          Hi, I'm <span className="text-[#915EFF]">Finn 👋</span>
-        </h1>
-        
-        {/* Location */}
-        <div className="flex items-center gap-2 mt-2 text-gray-700 dark:text-gray-300 text-lg">
-          <MapPinIcon className="h-6 w-6" />
-          <span>Gütersloh, Germany</span>
+      <span className="hash-span" id="AboutMe">
+        &nbsp;
+      </span>
+      <section className="relative w-full mx-auto">
+        <div className="inset-0 mt-12 max-w-7xl mx-auto px-8 flex flex-row items-start gap-6">
+          <div>
+            <h1 className="text-4xl sm:text-6xl font-black text-gray-900 dark:text-white leading-tight">
+              Hi, I'm{" "}
+              <span className="text-secondary">
+                Finn{" "}
+                <motion.span
+                  variants={wavingAnimation()} //todo: fix this --> why tf is not working???
+                  initial="hidden"
+                  animate="show"
+                  whileHover="hover"
+                >
+                  👋
+                </motion.span>
+              </span>
+            </h1>
+
+            {/* Location */}
+            <div className="flex items-center gap-2 mt-2 text-gray-700 dark:text-gray-300 text-lg">
+              <MapPinIcon className="h-6 w-6" />
+              <span>Gütersloh, Germany</span>
+            </div>
+
+            {/* Description */}
+            <p className="text-lg sm:text-xl font-medium mt-4 text-gray-600 dark:text-gray-400">
+              I'm a 21-year-old computer science bachelor's graduate with a
+              strong foundation in data science, machine learning, and AI.
+            </p>
+          </div>
         </div>
-        
-        {/* Description */}
-        <p className="text-lg sm:text-xl font-medium mt-4 text-gray-600 dark:text-gray-400">
-            I'm a 21-year-old computer science bachelor's graduate with a strong foundation in data science, machine learning, and AI.
-        </p>
-      </div>
-    </div>
-  </section>
-  <InterestsSection/>
+      </section>
     </>
   );
 };

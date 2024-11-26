38e1ea7e12d26574a15e2c70c335c57b0b0b935d
Tazah <finnbeifong@gmail.com>
2024-11-26 18:37:59 +0100

    change background

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
 import { BrowserRouter } from "react-router-dom";
-import {
-  Button,
-} from "@nextui-org/react";
-import { ArrowDownTrayIcon } from "@heroicons/react/20/solid";
 
 import { NavBar } from "./components/NavBar";
 import { AboutMe } from "./components/AboutMe";
+import { Work } from "./components/Work";
+import { InterestsSection } from "./components/InterestsSection";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className="bg-gray-100 dark:bg-[#14122a]">
-        <NavBar/>
-        <AboutMe/>
-        <Button radius="full" className="bg-gradient-to-tr from-pink-500 to-purple-500 text-white drop-shadow-lg" startContent={<ArrowDownTrayIcon className="w-5 h-6"/>}>
-          Download CV
-        </Button>
-        <ArrowDownTrayIcon/>
+      <div className="relative inset-0 bg-gray-100 dark:bg-[#14122a]">
+        <div
+          className="absolute inset-0 pointer-events-none"
+          style={{
+            backgroundImage: `url('/background.png')`,
+            backgroundRepeat: "repeat-y",
+            backgroundSize: "contain",
+            opacity: 0.2,
+            filter: "blur(5px)",
+          }}
+        ></div>
+        <NavBar />
+        <AboutMe />
+        <Work />
+        <InterestsSection />
       </div>
     </BrowserRouter>
   );
